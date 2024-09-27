16276e6fd5a7fd1cd85030f575bfae010edeb8ba
a7medbehiry <a7medbehiry@gmail.com>
2024-09-27 21:59:17 +0300

    code refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:test/features/chat/presentation/providers/chat_provider.dart';
